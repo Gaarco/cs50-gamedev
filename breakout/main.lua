778fcd4d8344a290d79dc5989362c9a4ff2bd6ba
Marco <marco.galli@protonmail.com>
2020-09-12 12:27:42 +0200

    Fix crash

--- breakout/main.lua
+++ breakout/main.lua
@@ -85,30 +85,30 @@ function loadHighScores()
         end
 
         love.filesystem.write("breakout.lst", scores)
-    else
-        local name = true
-        local currentName = nil
-        local counter = 1
-
-        local scores = {}
-
-        for i = 1, 10 do
-            scores[i] = {
-                name = nil,
-                score = nil
-            }
-        end
-
-        for line in love.filesystem.lines("breakout.lst") do
-            if name then
-                scores[counter].name = string.sub(line, 1, 3)
-            else
-                scores[counter].score = tonumber(line)
-                counter = counter + 1
-            end
-            name = not name
-        end
     end
+    local name = true
+    local currentName = nil
+    local counter = 1
+
+    local scores = {}
+
+    for i = 1, 10 do
+        scores[i] = {
+            name = nil,
+            score = nil
+        }
+    end
+
+    for line in love.filesystem.lines("breakout.lst") do
+        if name then
+            scores[counter].name = string.sub(line, 1, 3)
+        else
+            scores[counter].score = tonumber(line)
+            counter = counter + 1
+        end
+        name = not name
+    end
+    return scores
 end
 
 function love.update(dt)
