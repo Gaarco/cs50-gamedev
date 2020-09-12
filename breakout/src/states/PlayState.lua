ec101ca37d925e3fa3b54798c737e716589cc014
Marco <marco.galli@protonmail.com>
2020-09-12 15:51:35 +0200

    Maybe this is the last one, now everything should work

--- breakout/src/states/PlayState.lua
+++ breakout/src/states/PlayState.lua
@@ -1,146 +1,147 @@
 PlayState = Class{__includes = BaseState}
 
 function PlayState:enter(params)
-  self.paddle = params.paddle
-  self.bricks = params.bricks
-  self.health = params.health
-  self.score = params.score
-  self.highScores = params.highScores
-  self.ball = params.ball
-  self.level = params.level
+    self.paddle = params.paddle
+    self.bricks = params.bricks
+    self.health = params.health
+    self.score = params.score
+    self.highScores = params.highScores
+    self.ball = params.ball
+    self.level = params.level
 
-  -- give ball random starting velocity
-  self.ball.dx = math.random(-200, 200)
-  self.ball.dy = math.random(-50, -60)
+    -- give ball random starting velocity
+    self.ball.dx = math.random(-200, 200)
+    self.ball.dy = math.random(-50, -60)
 end
 
 function PlayState:update(dt)
-  if self.paused then
-    if love.keyboard.wasPressed("space") then
-      self.paused = false
-      gSounds["pause"]:play()
-    else
-      return
-    end
-  elseif love.keyboard.wasPressed("space") then
-    self.paused = true
-    gSounds["pause"]:play()
-    return
-  end
-
-  self.paddle:update(dt)
-  self.ball:update(dt)
-
-  if self.ball:collides(self.paddle) then
-    self.ball.y = self.paddle.y - 8
-    self.ball.dy = -self.ball.dy
-
-    if self.ball.x < self.paddle.x + (self.paddle.width / 2) and self.paddle.dx < 0 then
-      self.ball.dx = -50 + -(8 * (self.paddle.x + self.paddle.width / 2 - self.ball.x))
-    elseif self.ball.x > self.paddle.x + (self.paddle.width / 2) and self.paddle.dx > 0 then
-      self.ball.dx = 50 + (8 * math.abs(self.paddle.x + self.paddle.width / 2 - self.ball.x))
+    if self.paused then
+        if love.keyboard.wasPressed("space") then
+            self.paused = false
+            gSounds["pause"]:play()
+        else
+            return
+        end
+    elseif love.keyboard.wasPressed("space") then
+        self.paused = true
+        gSounds["pause"]:play()
+        return
     end
 
-    gSounds["paddle-hit"]:play()
-  end
+    self.paddle:update(dt)
+    self.ball:update(dt)
 
-  if self.ball.y >= VIRTUAL_HEIGHT then
-    self.health = self.health - 1
-    gSounds["hurt"]:play()
-
-    if self.health == 0 then
-      gStateMachine:change("game-over", {
-        score = self.score,
-        highScores = self.highScores,
-        level = self.level
-      })
-    else
-      gStateMachine:change("serve", {
-        paddle = self.paddle,
-        bricks = self.bricks,
-        health = self.health,
-        score = self.score,
-        highScores = self.highScores,
-        level = self.level
-      })
-    end
-  end
-
-  for k, brick in pairs(self.bricks) do
-    -- only check collision if we're in play
-    if brick.inPlay and self.ball:collides(brick) then
-      -- trigger the brick's hit function, which removes it from play
-      self.score = self.score + (brick.tier * 200 + brick.color * 25)
-
-      brick:hit()
-
-      if self:checkVictory() then
-        gSounds["victory"]:play()
-
-        gStateMachine:change("victory", {
-          level = self.level,
-          paddle = self.paddle,
-          health = self.health,
-          score = self.score,
-          ball = self.ball
-        })
-      end
-
-
-      if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
-        self.ball.dx = -self.ball.dx
-        self.ball.x = brick.x - 8
-      elseif self.ball.x + 6 > brick.x + brick.width and self.ball.dx < 0 then
-        self.ball.dx = -self.ball.dx
-        self.ball.x = brick.x + 32
-      elseif self.ball.y < brick.y then
+    if self.ball:collides(self.paddle) then
+        self.ball.y = self.paddle.y - 8
         self.ball.dy = -self.ball.dy
-        self.ball.y = brick.y - 8
-      else
-        self.ball.dy = -self.ball.dy
-        self.ball.y = brick.y + 16
-      end
 
-      self.ball.dy = self.ball.dy * 1.03
+        if self.ball.x < self.paddle.x + (self.paddle.width / 2) and self.paddle.dx < 0 then
+            self.ball.dx = -50 + -(8 * (self.paddle.x + self.paddle.width / 2 - self.ball.x))
+        elseif self.ball.x > self.paddle.x + (self.paddle.width / 2) and self.paddle.dx > 0 then
+            self.ball.dx = 50 + (8 * math.abs(self.paddle.x + self.paddle.width / 2 - self.ball.x))
+        end
+
+        gSounds["paddle-hit"]:play()
     end
-  end
 
-  for k, brick in pairs(self.bricks) do
-    brick.psystem:update(dt)
-  end
+    if self.ball.y >= VIRTUAL_HEIGHT then
+        self.health = self.health - 1
+        gSounds["hurt"]:play()
 
-  if love.keyboard.wasPressed("escape") then
-    love.event.quit()
-  end
+        if self.health == 0 then
+            gStateMachine:change("game-over", {
+                score = self.score,
+                highScores = self.highScores,
+                level = self.level
+            })
+        else
+            gStateMachine:change("serve", {
+                paddle = self.paddle,
+                bricks = self.bricks,
+                health = self.health,
+                score = self.score,
+                highScores = self.highScores,
+                level = self.level
+            })
+        end
+    end
+
+    for k, brick in pairs(self.bricks) do
+        -- only check collision if we're in play
+        if brick.inPlay and self.ball:collides(brick) then
+            -- trigger the brick's hit function, which removes it from play
+            self.score = self.score + (brick.tier * 200 + brick.color * 25)
+
+            brick:hit()
+
+            if self:checkVictory() then
+                gSounds["victory"]:play()
+
+                gStateMachine:change("victory", {
+                    level = self.level,
+                    paddle = self.paddle,
+                    health = self.health,
+                    score = self.score,
+                    highScores = self.highScores,
+                    ball = self.ball
+                })
+            end
+
+
+            if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x - 8
+            elseif self.ball.x + 6 > brick.x + brick.width and self.ball.dx < 0 then
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x + 32
+            elseif self.ball.y < brick.y then
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y - 8
+            else
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y + 16
+            end
+
+            self.ball.dy = self.ball.dy * 1.03
+        end
+    end
+
+    for k, brick in pairs(self.bricks) do
+        brick.psystem:update(dt)
+    end
+
+    if love.keyboard.wasPressed("escape") then
+        love.event.quit()
+    end
 end
 
 function PlayState:checkVictory()
-  for k, brick in pairs(self.bricks) do
-    if brick.inPlay then
-      return false
+    for k, brick in pairs(self.bricks) do
+        if brick.inPlay then
+            return false
+        end
     end
-  end
 
-  return true
+    return true
 end
 
 function PlayState:render()
-  for k, brick in pairs(self.bricks) do
-    brick:render()
-  end
+    for k, brick in pairs(self.bricks) do
+        brick:render()
+    end
 
-  for k, brick in pairs(self.bricks) do
-    brick:renderParticles()
-  end
+    for k, brick in pairs(self.bricks) do
+        brick:renderParticles()
+    end
 
-  self.paddle:render()
-  self.ball:render()
+    self.paddle:render()
+    self.ball:render()
 
-  renderScore(self.score)
-  renderHealth(self.health)
+    renderScore(self.score)
+    renderHealth(self.health)
 
-  if self.paused then
-    love.graphics.setFont(gFonts["large"])
-    love.graphics.printf("PAUSED", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, "center")
-  end
+    if self.paused then
+        love.graphics.setFont(gFonts["large"])
+        love.graphics.printf("PAUSED", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, "center")
+    end
 end
